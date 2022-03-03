78a5b2982e00b334de1bb022eba5016977db6eaa
tu7hnsxs1 <tu7hnsxs1@code.edu.az>
2022-03-04 01:05:36 +0400

    Task2 file

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (int i = 1; i <= 200; i++)
+            {
+               
+                if (i % 7 ==0 )
+                {
+
+                    count++;
+                    
+                }
+                
+
+
+            }
+            Console.WriteLine("Bu araliqda 7 e bolunen ededlerin sayi: = " + count);
+
+
+
+
+
+
+        }
+    }
+}
